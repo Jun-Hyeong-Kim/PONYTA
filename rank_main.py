43f730c9695fee5b0199aa2fe080d7a62d0b259d
Jun Hyeong Kim <tommy0906@snu.ac.kr>
2024-07-12 13:55:19 +0000

    rank_aggregation_func.py edited

--- rank_main.py
+++ rank_main.py
@@ -11,7 +11,8 @@ import sys
 import networkx as nx
 from sklearn.model_selection import train_test_split
 
-from rank_aggregation_func import rank_aggregation
+# from rank_aggregation_func import rank_aggregation
+import rank_aggregation_func
 from rank_prgenes_func import convert_to_int, rank_pr_genes
 from rank_eval_func import partial_roc, rank_evaluation
 
@@ -35,6 +36,10 @@ parser.add_argument('--np_pos_genes_count', type=int, help='number of np genes t
 parser.add_argument('--n_auc_min', type=int, default=5, help='minimum value for n')
 parser.add_argument('--n_auc_max', type=int, default=300, help='maximum value for n')
 
+
+parser.add_argument('--agg_method', type=str, default='weight_dibra', choices=['weight_dibra', 'median', 'linear_borda', 'majoritarian'], help='type of aggregation method')
+
+
 args = parser.parse_args()
 
 
@@ -42,7 +47,12 @@ args = parser.parse_args()
 
 ko_output = [folder for folder in os.listdir(args.ponyta_output) if 'npgenes' in folder][0]
 ranks_output = os.path.join(args.ponyta_output, ko_output)
-rank_aggregation(ranks_output)
+
+
+if args.agg_method in ['weight_dibra', 'linear_borda', 'majoritarian']:
+    rank_aggregation.rank_aggregation(output_path = ranks_output, agg_method = args.agg_method)
+elif args.agg_method == 'median':
+    rank_aggregation.rank_aggregation_median(ranks_output)
 
 
 # Ranks for Phenotype-related genes
