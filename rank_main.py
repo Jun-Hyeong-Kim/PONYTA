01452c58451b5970456f80e7aa83a3b74819909e
Jun Hyeong Kim <tommy0906@snu.ac.kr>
2024-07-12 15:02:18 +0000

    rank codes eidted

--- rank_main.py
+++ rank_main.py
@@ -11,11 +11,8 @@ import sys
 import networkx as nx
 from sklearn.model_selection import train_test_split
 
-# from rank_aggregation_func import rank_aggregation
-import rank_aggregation_func
-from rank_prgenes_func import convert_to_int, rank_pr_genes
-from rank_eval_func import partial_roc, rank_evaluation
-
+import rank_aggregation_func, rank_prgenes_func, rank_eval_func
+# from rank_eval_func import partial_roc, rank_evaluation
 
 
 parser = argparse.ArgumentParser()
@@ -30,12 +27,12 @@ parser.add_argument('--deg_output_path', type=str, help='DEG output path')
 parser.add_argument('--np_output_path', type=str, help='NP output path')
 parser.add_argument('--nx_network_path', type=str, help='Path to networkx file')
 
+parser.add_argument('--deg_pos_genes_count', type=int, help='number of degs to use')
 parser.add_argument('--np_pos_genes_count', type=int, help='number of np genes to use')
 
 # rank evaluation
 parser.add_argument('--n_auc_min', type=int, default=5, help='minimum value for n')
 parser.add_argument('--n_auc_max', type=int, default=300, help='maximum value for n')
-
 
 parser.add_argument('--agg_method', type=str, default='weight_dibra', choices=['weight_dibra', 'median', 'linear_borda', 'majoritarian'], help='type of aggregation method')
 
@@ -57,8 +54,8 @@ elif args.agg_method == 'median':
 
 # Ranks for Phenotype-related genes
 
-rank_pr_genes(ranks_output, args.pr_genes_path, args.deg_output_path, args.np_output_path, args.nx_network_path, args.np_pos_genes_count)
+rank_pr_genes_func.rank_pr_genes(ranks_output, args.pr_genes_path, args.deg_output_path, args.np_output_path, args.nx_network_path, args.deg_pos_genes_count, args.np_pos_genes_count, args.agg_method)
 
 
 # Rank Evaluation
-rank_evaluation(ranks_output, np_pos_genes_count, args.n_auc_min, args.n_auc_max)
+rank_eval_func.rank_evaluation(ranks_output, np_pos_genes_count, args.n_auc_min, args.n_auc_max, args.pr_genes_path, args.nx_network_path, args.agg_method)
